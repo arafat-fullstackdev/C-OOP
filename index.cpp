d84626e999dc2f29a30653435284d96e4b39e3aa
Arafat Patwary <farabipsk@gmail.com>
2025-09-17 22:46:48 +0600

    Power c

--- index.cpp
+++ index.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main (){
     cout<<"Hello CPP"<<endl;
+    //double base = 5.0;
+    double exponent = 3.0;
+    double result = pow(base, exponent); // Calculates 5 raised to the power of 3
+
+    scout << Result:<<endl.
     return 0;
+
 }
