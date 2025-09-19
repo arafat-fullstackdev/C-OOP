e48c702e358e3701039e058ebc473069ebe8f1ea
Arafat Patwary <farabipsk@gmail.com>
2025-09-19 17:14:55 +0600

    Donation Drive prob 500

--- index.cpp
+++ index.cpp
@@ -7,7 +7,25 @@ int main (){
     double exponent = 3.0;
     double result = pow(base, exponent); // Calculates 5 raised to the power of 3
 
-    scout << Result:<<endl.
+    scout << Result:<<endl;
+    //Donation Drive
+    #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n,x,t;
+
+cin>>t;
+
+while(t--){
+    cin>>n>>x;
+    cout<<n-x<<endl;
+    
+}
+return 0;
+}
+
     return 0;
 
 }
