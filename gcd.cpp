beaac31530d48c211bcdcfd32322b4cb8a447e12
Arafat Patwary <farabipsk@gmail.com>
2025-09-24 16:57:49 +0600

    Code for gcd

---
+++ gcd.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+
+    int result = min(a,b);
+
+    while (result>1){
+     
+        if(a % result == 0 && b %  result == 0 ){
+            break;
+            result--;
+        }
+       
+    }
+   return result;
+    
+
+}
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    cout<<gcd(a,b)<<endl;
+    return 0;
+}
