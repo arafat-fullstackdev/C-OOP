ca1fd5206ab84a817ca6bee539ff1a5047130128
Arafat Patwary <farabipsk@gmail.com>
2025-09-26 20:54:05 +0600

    Stack array implement

--- stack.cpp
+++ stack.cpp
@@ -2,36 +2,22 @@
 #include <stack>
 using namespace std;
 
-int celebrity(vector<vector<int>> & mat) {
-    int n = mat.size();
+class arrayStack{
+    int arr;
 
-    // indegree and outdegree array
-    vector<int> indegree(n, 0), outdegree(n, 0);
+    // array size
+    int capacity;
+    //array element index;
+    int top;
 
-    // query for all edges
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int x = mat[i][j];
-
-            // set the degrees
-            outdegree[i] += x;
-            indegree[j] += x;
-        }
+    public:
+    arrayStack(int cap){
+  capacity = cap;
+  top = -1;
     }
-
-    // find a person with indegree n-1
-    // and out degree 0
-    for (int i = 0; i < n; i++)
-        if (indegree[i] == n && outdegree[i] == 1)
-            return i;
-
-    return -1;
 }
-
 int main() {
-    vector<vector<int> > mat = {{ 1, 1, 0 },
-                                { 0, 1, 0 },
-                                { 0, 1, 1 }};
-    cout << celebrity(mat);
+   int a;
+   
     return 0;
 }
