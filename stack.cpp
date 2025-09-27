d427a4e750e4af1f977fcaac9a3d7d7a2548e295
Arafat Patwary <farabipsk@gmail.com>
2025-09-27 08:54:58 +0600

    Use array in Stack

--- stack.cpp
+++ stack.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class arrayStack{
-    int arr;
+    int *arr;
 
     // array size
     int capacity;
@@ -11,13 +11,44 @@ class arrayStack{
     int top;
 
     public:
+    //constructor implement
     arrayStack(int cap){
   capacity = cap;
   top = -1;
-    }
-}
+  arr = new  int[capacity];
+    };
+
+
+
+//push operation
+
+void push(int n){
+                 if(top == capacity -1){
+                  cout<<"Stack Overflow!"<<endl;
+                  return ;
+                 }
+
+                 arr[++top] =n;
+};
+
+int pop(){
+  if(top == -1){
+    cout<<"Stack Underflow!"<<endl;
+
+    return -1;
+  }
+};
+};
 int main() {
-   int a;
-   
+arrayStack st(5);
+
+st.push(1);   
+st.push(11);   
+st.push(111);   
+st.push(11111);   
+st.push(100000); 
+
+cout<<"Poped value"<<st.pop()<<endl;
+
     return 0;
 }
