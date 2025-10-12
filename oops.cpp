5f6efc1d3a41df05a0b1cedd1503c7fe29fc41fa
Arafat Patwary <farabipsk@gmail.com>
2025-10-12 10:45:01 +0600

    OOP practice

---
+++ oops.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>;
+using namespace std;
+
+class Teacher{
+ 
+    public:
+    //properties // data members
+    string name;
+    string dept;
+    string sub;
+    double salary;
+    
+    //methods/member functions
+
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+};
+
+int main(){
+Teacher t1;
+t1.name = "Dimitri";
+t1.dept = "C++";
+t1.sub = "OOP";
+t1.salary = 10000;
+
+cout<<t1.name<<endl;
+
+Teacher t2;
+Teacher t3;
+
+}
