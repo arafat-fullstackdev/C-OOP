f0008dc7245bc76a72b95ee4865c8bf5f49ba53e
Arafat Patwary <farabipsk@gmail.com>
2025-10-15 15:39:50 +0600

    Vactor Capacity code

---
+++ STL/vector.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // vector<int> vc; //type 1
+    // vector<int> vc(22); //type 2
+    vector<int> vc(5,22); //type 3
+
+    for(int  i =0; i<vc.size(); i++){
+        cout<<vc[i]<<" ";
+    }
+    cout<<vc.size()<<endl;
+    cout<<vc.size()<<endl;
+    cout<<vc.size()<<endl;
+    return 0;
+}
