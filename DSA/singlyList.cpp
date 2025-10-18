93ce96da8cbb2fe57420625355aa20273cf0a2eb
Arafat Patwary <farabipsk@gmail.com>
2025-10-18 18:40:59 +0600

    Code Singly

--- DSA/singlyList.cpp
+++ DSA/singlyList.cpp
@@ -47,7 +47,53 @@ int getMiddleIndex(Node* head){
     return head->data;
     
 }
+//Copy code
 
+struct node {
+   int data;
+   struct node *next;
+};
+struct node *head = NULL;
+struct node *current = NULL;
+
+// display the list
+void printList(){
+   struct node *p = head;
+   printf("\n[");
+   
+   //start from the beginning
+   while(p != NULL) {
+      printf(" %d ",p->data);
+      p = p->next;
+   }
+   printf("]");
+}
+
+//insertion at the beginning
+void insertatbegin(int data){
+   
+   //create a link
+   struct node *lk = (struct node*) malloc(sizeof(struct node));
+   lk->data = data;
+   
+   // point it to old first node
+   lk->next = head;
+   
+   //point first to new first node
+   head = lk;
+}
+void copyCode(){
+   int k=0;
+   insertatbegin(12);
+   insertatbegin(22);
+   insertatbegin(30);
+   insertatbegin(44);
+   insertatbegin(50);
+   printf("Linked List: ");
+   
+   // print list
+   printList();
+}
 int main() {
 
     Node* head = new Node(10);
