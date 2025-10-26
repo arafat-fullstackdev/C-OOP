805f07339a4dd17f2b16f5a9ef377b6e9c7c4c91
Arafat Patwary <farabipsk@gmail.com>
2025-10-26 23:47:01 +0600

    practice vector

--- STL/string_vector.cpp
+++ STL/string_vector.cpp
@@ -3,19 +3,31 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    vector<string> v;
-    for (int i = 0; i < n; i++)
-    {
-        string s;
-        cin >> s;
-        v.push_back(s);
-    }
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << endl;
-    }
+    // int n;
+    // cin >> n;
+    // vector<string> v;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     string s;
+    //     cin >> s;
+    //     v.push_back(s);
+    // }
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << endl;
+    // }
+
+    //? vector Access
+     vector<int> vc={1,2,3,4,5,6};
+     vector<char> cht={'a','b','c','d'};
+
+     cout<<"Vector Access: "<<vc[1]<<endl;
+     cout<<"Vector Access: "<<vc[0]<<endl;
+     cout<<"Vector Access: "<<vc[3]<<endl;
+     //Traversing a vector  with loop
+     for(int i =0; i<cht.size(); i++){
+        cout<<"Vector traverse "<<cht[i]<<endl;
+     }
 
     return 0;
 };
