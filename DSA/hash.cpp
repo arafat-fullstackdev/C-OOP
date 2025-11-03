f6a93c606448a713de9aaf2821d69884342fb348
Arafat Patwary <farabipsk@gmail.com>
2025-11-03 15:22:15 +0600

    Implement hasting

---
+++ DSA/hash.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    string sc = "greeksForGeeks";
+    string sc1 = "geeksMethodology";
+
+    hash<string> hs;
+    hash<string> hs1;
+    cout<<hs(sc)<<endl;
+    cout<<hs1(sc1)<<endl;
+
+return 0;
+
+};
