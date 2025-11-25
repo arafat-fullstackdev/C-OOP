2292eb1c9e2ef18c0a8ca95014e7021388a984de
Arafat Patwary <farabipsk@gmail.com>
2025-11-25 18:04:47 +0600

    CSES

---
+++ CSES/print.cpp
@@ -0,0 +1,24 @@
+//#include <bits/stdc++.h>;
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long  n;
+
+        cin >> n;
+
+        if (n % 2 == 0)
+            cout << n / 2 << endl;
+        else
+            cout << (n * 3) + 1 << endl;
+    }
+
+    return 0;
+};
