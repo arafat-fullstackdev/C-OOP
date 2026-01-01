da2cb71e77a36afc8c77475fec90ee2b8f82b6b2
Arafat Patwary <farabipsk@gmail.com>
2026-01-01 18:18:04 +0600

    Explore cp

--- geeks/check.cpp
+++ geeks/check.cpp
@@ -6,10 +6,17 @@ void printTable(int n){
         cout<<n<<" * "<<i<<"="<<n*i<<endl;
     };
 }
+void printTable1(int m){
+    for(int i =1; i<=10; i++){
+        cout<<m<<" * "<<i<<"="<<m*i<<endl;
+    };
+}
 
 int main(){
   int n =6;
  printTable(6);
+ int m =7;
+ printTable1(7);
 return 0;
 
 };
